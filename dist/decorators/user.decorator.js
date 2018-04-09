0f971dcbadaf58dfd814877006d1589fb87d6151
Kyle Pfromer <kpfromer2@gmail.com>
2018-04-09 17:48:06 -0600

    Remove dist

--- dist/decorators/user.decorator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@nestjs/common");
-exports.User = common_1.createRouteParamDecorator((data, req) => {
-    return req.user;
-});
-//# sourceMappingURL=user.decorator.js.map
