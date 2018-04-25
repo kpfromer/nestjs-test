bd296f986d3c3f7f303b9084d8a492f0f27df495
Kyle Pfromer <kpfromer2@gmail.com>
2018-04-24 21:10:58 -0600

    Remove CatModule

--- src/modules/cat/cat.model.ts
+++
@@ -1,8 +0,0 @@
-import { prop, Typegoose } from 'typegoose';
-import {IsString} from 'class-validator';
-
-export class Cat extends Typegoose {
-  @IsString()
-  @prop({ required: true })
-  name: string;
-}
